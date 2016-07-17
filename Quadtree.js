d20a8ffec1f2bca7078c18eedd4dc78a7fe48c77
Andrew54757 <andrews54757@gmail.com>
2016-07-17 14:31:42 -0400

    Create Quadtree.js

---
+++ Quadtree.js
@@ -0,0 +1,18 @@
+"use strict"
+const FastMap = require('collections/fast-map');
+
+module.exports = class QuadTree {
+constructor(top,bottom,left,right, level,parent) {
+this.top = top;
+this.bottom = bottom;
+this.left = left;
+this.right = right;
+
+
+}
+
+
+
+
+
+}
