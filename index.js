d4a3209f69e3e0fa8019c900c1dd4582980608eb
Andrew54757 <andrews54757@gmail.com>
2016-07-17 14:43:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = class QuadTreeMap {
     this.left = left;
     this.right = right;
     this.positionkey = "position";
-    this.quadtree = new QuadTree(top,bottom,left,right,0,false);
+    this.quadtree = new QuadTree(top,bottom,left,right,0,false,this.positionkey);
   }
   get(key) {
     
