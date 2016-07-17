5fddfe9b80750b64582750b478408a428937112a
Andrew54757 <andrews54757@gmail.com>
2016-07-17 14:28:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-
+var positionkey = "position";
+module.exports = {
+  get: function(key) {
+    
+    
+  },
+  set: function(key,data) {
+    
+    
+  },
+  forEach: function(callback) {
+    
+    
+  },
+  every: function(callback) {
+    
+    
+  },
+  keys: function() {
+    
+    
+  },
+  getQuad: function(node) {
+    
+    
+  },
+  setPosVar: function(var) {
+    
+    
+  },
+  
+  
+  
+}
