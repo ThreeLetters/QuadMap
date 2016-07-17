1d256456a0222a4343b7b97fc837d68d356eeaff
Andrew54757 <andrews54757@gmail.com>
2016-07-17 14:52:04 -0400

    Update Quadtree.js

--- Quadtree.js
+++ Quadtree.js
@@ -2,16 +2,29 @@
 const FastMap = require('collections/fast-map');
 
 module.exports = class QuadTree {
-constructor(top,bottom,left,right, level,parent) {
+constructor(top,bottom,left,right, level,parent,positkey) {
 this.top = top;
 this.bottom = bottom;
 this.left = left;
 this.right = right;
-
+this.quads = [];
+this.nodes = new FastMap();
+this.positionkey = (!positkey && parent.positionkey) ? parent.positionkey : positkey;
 
 }
-getQuad(node) {
+doesFit(position) {
+  var x = position.x;
+  var y = position.y;
+  if (x < this.left) return false;
+  if (x >= this.right) return false;
+  if (y < this.bottom) return false;
+  if (y >= this.top) return false;
+  return true;
   
+}
+getQuad(node,box) {
+  if (!node[this.positionkey]) return false;
+  this.doesFit(node[this.positionkey])
   
 }
 
