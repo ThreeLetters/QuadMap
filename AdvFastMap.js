f89e6fbe65028075f85b2abacebcece157b0d5f8
Andrew54757 <andrews54757@gmail.com>
2016-07-25 08:42:27 -0700

    Update AdvFastMap.js

--- AdvFastMap.js
+++ AdvFastMap.js
@@ -6,6 +6,20 @@ this.ma = ma;
 this.vari = vari;
 }
 getNodes (progressive,keep) {
+if (progressive) {
+  var ns = new FastMap();
+  this.forEach((node,id)=>{
+    if (keep) ns.set(id,node); else ns.set(id,node.node);
+    
+  });
+  this.ma.quads.forEach((quad)=>{
+    ns.concat(quad[this.vari].getNodes(progressive,keep));
+    
+  });
+  return ns;
+} else {
+  return (keep) ? this.allnodes : this.allNodes;
+}
 
-
+}
 }
