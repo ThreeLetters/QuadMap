c7650811418abc6dc8343620459548eed4aa200e
Andrew54757 <andrews54757@gmail.com>
2016-07-25 08:36:13 -0700

    Create AdvFastMap.js

---
+++ AdvFastMap.js
@@ -0,0 +1,11 @@
+"use strict";
+const FastMap = require('collections/fast-map');
+module.exports = class AdvFast extends FastMap {
+constructor(ma,vari) {
+this.ma = ma;
+this.vari = vari;
+}
+getNodes (progressive,keep) {
+
+
+}
