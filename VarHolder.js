69a70779d419944ba2b7d42b08f4da5ae993b060
Andrew54757 <andrews54757@gmail.com>
2016-07-25 07:26:49 -0700

    Create VarHolder.js

---
+++ VarHolder.js
@@ -0,0 +1,23 @@
+module.exports = class VarHolder {
+constructor(main,varn) {
+this.main = main;
+this.varn = varn;
+}
+set(id,node) {
+
+}
+get(id) {
+
+}
+delete(id,node) {
+
+
+}
+toArray() {
+
+}
+
+
+
+
+}
